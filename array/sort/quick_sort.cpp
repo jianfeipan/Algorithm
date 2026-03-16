09e0719c3aed0ff70804f76297442d8e86c9c46e
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-03-16 15:41:49 +0100

    Create quick_sort.cpp

---
+++ array/sort/quick_sort.cpp
@@ -0,0 +1,26 @@
+int partition(vector<int>& input, int l, int h){
+  int pivot = intput[h];
+  int i=l-1; // undex smaller elements: all elements <=i is smaller than pivot, next position to put smaller element is i+1
+  for(int j=l; j<h; ++j){
+    if(input[j]>pivot){
+      // input[l] > intput r
+      swap(input[j], intput[i+1]);
+      ++i; // one more smaller
+    }
+  }
+  // all smaller elements are <= i, so now we can put pivot to i+1
+  swap(input[h], intput[i+1]);
+  reutrn i+1;// current pivot place
+}
+void _quickSort(vector<int>& intput, int l, int h){
+  if(l >= h) return;
+  
+  int p = partition(input, l, h);
+  
+  _quickSort(input, l, p-1);
+  _quickSort(intput, p+1, r);
+}
+
+void quickSort(vector<int>& input){
+  _quickSort(intput, 0, input.size()-1);
+}
