0439529b29f065fbca7a72965e43ced9c55cf4fc
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-03-18 09:22:01 +0100

    Create leet 122. Best Time to Buy ans Sell Stock II multi trades.cpp

---
+++ DP/leet 122. Best Time to Buy ans Sell Stock II multi trades.cpp
@@ -0,0 +1,58 @@
+/*
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can sell and buy the stock multiple times on the same day, ensuring you never hold more than one share of the stock.
+
+Find and return the maximum profit you can achieve.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Total profit is 4 + 3 = 7.
+Example 2:
+
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Total profit is 4.
+Example 3:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 3 * 104
+0 <= prices[i] <= 104
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        const auto n = prices.size();
+        vector<int> buy(n);
+        vector<int> sell(n);
+        vector<int> hold(n);
+        vector<int> wait(n);
+
+        buy[0] = -prices[0];
+        sell[0] = INT_MIN; // no possible on day 0
+        hold[0] = INT_MIN; // no possible on day 0
+        wait[0] = 0;
+        for(int i=1; i<n; ++i){
+            buy[i] = max(wait[i-1], sell[i-1]) - prices[i];
+            sell[i] = max(buy[i-1], hold[i-1]) + prices[i];
+            hold[i] = max(buy[i-1], hold[i-1]);
+            wait[i] = max(sell[i-1], wait[i-1]);
+        }
+
+        return max(sell[n-1], wait[n-1]);
+    }
+};
