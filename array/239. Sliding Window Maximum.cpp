890e22c89ce2057bd68fe5e0627fdfcadebaed9c
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-03-22 22:12:27 +0100

    Update 239. Sliding Window Maximum.cpp

--- array/239. Sliding Window Maximum.cpp
+++ array/239. Sliding Window Maximum.cpp
@@ -25,69 +25,29 @@ Constraints:
 */
 
 class Solution {
+/*
+ideas:
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+BF: O(n*k)
+ when I move window: using a multi-set to keep order of K elements, update it and get max
+ O(nlogK)
+*/
 public:
-
-   
-
-
-    //use a tree map to keep the order, so we have O(N*logK)
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        //k = 3
-        // l
-        // 0 1 2 3 4
-        // l.  j
-        vector<int> res;res.reserve(nums.size() - k +1);
-        map<int, int> ordered;
-
+        vector<int> windowMax;windowMax.reserve(nums.size()-k+1);
+        multiset<int> ordered;
         for(int i=0; i<k; ++i){
-            ++ordered[nums[i]];
+            ordered.insert(nums[i]);
         }
-        int left = 0;
-        while(left+k<=nums.size()){
-            res.push_back(ordered.rbegin()->first);
-            --ordered[nums[left]];
-            if(ordered[nums[left]] == 0){
-                ordered.erase(nums[left]);
-            }
-            ++ordered[nums[left+k]];
-            ++left;
+        windowMax.push_back(*ordered.rbegin());
+
+        for(int i=1; i+k-1<nums.size(); ++i){
+            ordered.erase(ordered.find(nums[i-1]));
+            ordered.insert(nums[i+k-1]);
+            
+            windowMax.push_back(*ordered.rbegin());
         }
-        return res;
-    }
-
-    // using heap solution has one problem: how do you know you top is out of your window or not? 
-    // the solution is to combine the index to the pair then push it to the heap, 
-    // then you can get the pop util th's in your range to have the max in the window
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        priority_queue<pair<int, int>> heap;
-        vector<int> output;
-        for (int i = 0; i < nums.size(); i++) {
-            heap.push({nums[i], i});
-            if (i >= k - 1) {
-                while (heap.top().second <= i - k) {
-                    heap.pop();
-                }
-                output.push_back(heap.top().first);
-            }
-        }
-        return output;
-    }
-
-
-    //O(N*K)
-    vector<int> maxSlidingWindow_BF(vector<int>& nums, int k) {
-        //k = 3
-        // l
-        // 0 1 2 3 4
-
-        vector<int> res;res.reserve(nums.size() - k +1);
-        for(int left = 0; left+k<=nums.size(); ++left){
-            int localMax = INT_MIN;
-            for(int i = left; i<left+k; ++i){
-                localMax = max(localMax, nums[i]);
-            }
-            res.push_back(localMax);
-        }
-        return res;
+        return windowMax;
     }
 };
