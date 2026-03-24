b3b1592d276b3905d5aaf0dd559b72bae5f4f5e2
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-03-24 23:39:05 +0100

    Create 225. Implement Stack using Queues.cpp

---
+++ stack/225. Implement Stack using Queues.cpp
@@ -0,0 +1,41 @@
+class MyStack {
+private:
+    queue<int> q; // front, pop, push
+
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        int n = q.size();
+        for(int i = 1; i<=n-1; ++i){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int top = q.front();
+        q.pop();
+        return top;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
