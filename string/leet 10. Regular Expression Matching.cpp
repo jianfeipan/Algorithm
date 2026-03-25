2f3cd17b92d0f6279f94d346d7163a12c4b63809
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-03-25 23:20:03 +0100

    Update leet 10. Regular Expression Matching.cpp

--- string/leet 10. Regular Expression Matching.cpp
+++ string/leet 10. Regular Expression Matching.cpp
@@ -1,18 +1,52 @@
-class Solution {
-public:
-    bool isMatch(string s, string p) 
-    {
-        if (p.empty()) return s.empty();
-        bool first_match = (!s.empty() &&
-                               (p[0] == s[0] || p[0] == '.'));
+/*
+'.' Matches any single character
+'*' Matches zero or more of the preceding element.
 
-        if (p.size() >= 2 && p[1] == '*')
-        {
-            return (isMatch(s, p.substr(2)) ||
-                    (first_match && isMatch(s.substr(1), p)));
-        } else 
-        {
-            return first_match && isMatch(s.substr(1), p.substr(1));
+idea:
+    two pointers
+    using a recursive way: 
+        if match 
+            following * ? 
+             yes->  skip star j+2 or skip letter i+1
+             no-> i+1, j+i
+         if not match
+             following * ?
+                 yes -> skip * j+2
+                 no -> false ! miss
+            
+
+
+
+*/
+
+class Solution {
+private:
+    bool dfs(const string& s, const string& p, int i, int j){
+        if(p.size() == j) return s.size() == i;
+        if(s.size() == i){ // s is over
+            if(j+1<p.size() && p[j+1] == '*') // following a *
+                return dfs(s, p, i, j+2); // only .* a* b* left
         }
+
+        if(s[i] == p[j] || p[j] == '.'){ // match
+            if(j+1<p.size() && p[j+1] == '*'){ // following a *
+                return dfs(s, p, i+1, j)  // skip the current letter try i+1
+                    || dfs(s, p, i, j+2); // skip the .* patern try j+2
+            }
+            return dfs(s, p, i+1, j+1); // no *, single letter match
+        }else{ // miss
+            if(j<p.size()-1 && p[j+1] == '*'){ //  following a *
+                return dfs(s, p, i, j+2); // skip the .*
+            }
+            
+            return false; // no * single letter miss match
+        }
+
+
+
+    }
+public:
+    bool isMatch(string s, string p) {
+        return dfs(s, p, 0, 0);
     }
 };
