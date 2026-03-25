d98dca33b03bddcc1d720eedafe23f6df15f89d8
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-03-25 13:47:52 +0100

    Update 212. Word Search II.cpp

--- backtracking/212. Word Search II.cpp
+++ backtracking/212. Word Search II.cpp
@@ -1,147 +1,184 @@
-
-#include<array>
-class Solution_loop_word_dfs_back_track {
 /*
-dfs to find word (length = w) board: (m*n)
-loop on words (l words):
-time O( l*m*n*4*3^(w-1) )
-space: O(m*n+w*l)
+
+212. Word Search II
+
+Given an m x n board of characters and a list of strings words, return all words on the board.
+
+Each word must be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring.
+The same letter cell may not be used more than once in a word.
+
+
+
+Example 1:
+
+
+Input: board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], words = ["oath","pea","eat","rain"]
+Output: ["eat","oath"]
+Example 2:
+
+
+Input: board = [["a","b"],["c","d"]], words = ["abcb"]
+Output: []
+
+
 */
+/*
+idea:
+    loop on words
+    -> findWord()
+        loop on the board find match of the first letter
+        from that one DFS
+            findWordFrom(current position, current letter index, word, visited)
+                when rach the end -> return true
+
+
+
+*/
+constexpr array<array<int, 2>, 4> DIRECTIONS{{
+    {1, 0},
+    {-1, 0},
+    {0, 1},
+    {0, -1}
+}};
+class Solution_dfs {
 private:
-    bool _dfs(const vector<vector<char>>& board, 
-                const string& word,
-                int pos,
-                vector<vector<char>>& visited, 
-                int r, int c)
-        {
-            if(pos == word.size()) return true;
-            if(r<0 || r>=board.size() || c<0 || c>=board[r].size()) 
-                return false;
-            if(visited[r][c] == '.' || board[r][c] != word[pos]) 
-                return false;
-            
-            visited[r][c] = '.';
-            if(_dfs(board, word, pos+1, visited, r+1, c)
-            || _dfs(board, word, pos+1, visited, r-1, c)
-            || _dfs(board, word, pos+1, visited, r, c+1)
-            || _dfs(board, word, pos+1, visited, r, c-1))
-                return true;
+    bool dfs(const vector<vector<char>>& board, const string& word,
+            int index, int row, int col, vector<vector<bool>>& visited)
+    {
+        if(index == word.size()) return true;
 
-            visited[r][c] = word[pos];
-            return false;
-        }
-
-
-    bool _find(const vector<vector<char>>& board, const string& word){
-        for(int r = 0; r<board.size(); ++r){//m*n
-            for(int c = 0; c<board[r].size(); ++c){
-                vector<vector<char>> visited = board;
-                if(_dfs(board, word, 0, visited, r, c)){//3^w
-                    return true;
-                }
+        for(const auto& [dr, dc] : DIRECTIONS){
+            int r = row+dr;
+            int c = col+dc;
+            if(r<board.size() && r>=0
+               && c<board[0].size() && c>=0
+               && !visited[r][c]
+               && board[r][c] == word[index]){
+                visited[r][c] = true;
+                if(dfs(board, word, index+1, r, c, visited)) return true;
+                visited[r][c] = false;
             }
         }
         return false;
     }
+
+    bool findWord(const vector<vector<char>>& board, const string& word){
+        for(int r=0; r<board.size(); ++r){
+            for(int c=0; c<board[r].size(); ++c){
+                if(board[r][c] == word[0]){
+                    vector<vector<bool>> visited(board.size(), vector<bool>(board[0].size(), false));
+                    visited[r][c] = true;
+                    if(dfs(board, word, 1, r, c, visited)){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
 public:
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
         vector<string> found;
-        for(const auto& word:words){//L
-            if(_find(board, word)){
+
+        for(const auto& word : words){
+            if(findWord(board, word)){
                 found.push_back(word);
             }
         }
+
         return found;
     }
 };
-/*
 
-optimization: 
+class Solution{// Trie + DFS
+struct TrieNode {
+    array<TrieNode*, 26> children{};
+    string* word = nullptr; // store the word at the end node
+};
 
-looping in words is not optimal: 
-ab, and a, share the same prefix -> prefix tree.
-so let's store the input words in a prfix tree, and we can do the dft on the tree, when we got a word end,
-we add the word into the result
-*/
-
-class Solution{
-    private:
-
-    struct Node{
-        array<Node*, 26> nexts;
-        int wordIndex;
-        int refs;//avoid duplications ??
-        Node():wordIndex(-1),refs(0){
-            nexts.fill(nullptr);
-        }
-    };
-
-    void _dfs(Node* current,
-                const vector<vector<char>>& board, 
-                const vector<string>& words,
-                vector<vector<char>>& visited, 
-                int r, int c,
-                vector<string>& found)
-        {
-            if(r<0 || r>=board.size() || c<0 || c>=board[r].size()) 
-                return;
-            if(visited[r][c] == '.') 
-                return;
-            Node* next = current->nexts[board[r][c] - 'a'];
-            if(!next) return;
-
-            visited[r][c] = '.';
-            if(next->wordIndex != -1){
-                found.push_back(words[next->wordIndex]);
-                next->wordIndex = -1;
-                // without following also passed all tests ???
-                // next->refs--;
-                // if (!next->refs) {
-                //     current->nexts[board[r][c] - 'a'] = nullptr;
-                //     visited[r][c] = board[r][c];
-                //     return;
-                // }
+private:
+    void insertWord(TrieNode* root, string& word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            int index = c - 'a';
+            if (!node->children[index]) {
+                node->children[index] = new TrieNode();
             }
-
-            _dfs(next, board, words, visited, r+1, c, found);
-            _dfs(next, board, words, visited, r-1, c, found);
-            _dfs(next, board, words, visited, r, c+1, found);
-            _dfs(next, board, words, visited, r, c-1, found);
-
-            visited[r][c] = board[r][c];
+            node = node->children[index];
+        }
+        node->word = &word;
+    }
+    void dfs(const vector<vector<char>>& board, TrieNode* node,
+            int row, int col, vector<vector<bool>>& visited, vector<string>& found)
+    {   
+        if(!node) return;
+        
+        if(node->word){
+            found.push_back(*node->word);
+            node->word = nullptr; // avoid duplicate
         }
 
-
-    void _find(Node* root, const vector<vector<char>>& board, const vector<string>& words, vector<string>& found){
-        for(int r = 0; r<board.size(); ++r){//m*n
-            for(int c = 0; c<board[r].size(); ++c){
-                vector<vector<char>> visited = board;
-                _dfs(root, board, words, visited, r, c, found);
+        for(const auto& [dr, dc] : DIRECTIONS){
+            int r = row+dr;
+            int c = col+dc;
+            if(r<board.size() && r>=0
+               && c<board[0].size() && c>=0
+               && !visited[r][c]){
+                char nextChar = board[r][c];
+                int index = nextChar - 'a';
+                if(node->children[index]){
+                    visited[r][c] = true;
+                    dfs(board, node->children[index], r, c, visited, found);
+                    visited[r][c] = false;
+                }
             }
         }
     }
 
-    void _add(Node* last, const vector<string>& words, int index){
-        last->refs++;//?????
-        for(char c : words[index]){
-            int currentLetter = c - 'a';
-            if(nullptr == last->nexts[currentLetter]){
-                last->nexts[currentLetter] = new Node();
-            }
-            last = last->nexts[currentLetter];
-            last->refs++;
-        }
-        last->wordIndex = index;// if the index is not -1, it's a word, and the word is words[index]
-    }
 public:
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        Node root;
-        for(int i=0; i<words.size(); ++i){//L
-            _add(&root, words, i);
-        }
+        const auto ROW = board.size();
+        const auto COL = board[0].size();
 
         vector<string> found;
-        _find(&root, board, words, found);
+
+        TrieNode* root = new TrieNode();
+        for(auto& word : words){
+            insertWord(root, word);
+        }
+
+        vector<vector<bool>> visited(ROW, vector<bool>(COL, false));
+        for(int r=0; r<ROW; ++r){
+            for(int c=0; c<COL; ++c){
+                char ch = board[r][c];
+                int index = ch - 'a';
+                if(root->children[index]){
+                    visited[r][c] = true;
+                    dfs(board, root->children[index], r, c, visited, found);
+                    visited[r][c] = false;
+                }
+            }
+        }
+
         return found;
     }
 };
+
+/*
+
+board =
+[["a","b","c"],
+ ["a","e","d"],
+ ["a","f","g"]]
+words =
+["abcdefg","gfedcbaaa","eaabcdgfa","befa","dgc","ade"]
+
+Use Testcase
+Output
+["abcdefg","gfedcbaaa","befa"]
+Expected
+["abcdefg","befa","eaabcdgfa","gfedcbaaa"]
+
+*/
