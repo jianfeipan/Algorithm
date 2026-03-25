b31af3682643f09bf298cc26b65cc436f46b7998
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-03-25 18:22:51 +0100

    Create 494. target_sum.cpp

---
+++ DP/494. target_sum.cpp
@@ -0,0 +1,61 @@
+/*
+You are given an array of integers nums and an integer target.
+
+For each number in the array, you can choose to either add or subtract it to a total sum.
+
+For example, if nums = [1, 2], one possible sum would be "+1-2=-1".
+If nums=[1,1], there are two different ways to sum the input numbers to get a sum of 0: "+1-1" and "-1+1".
+
+Return the number of different ways that you can build the expression such that the total sum equals target.
+
+Example 1:
+
+Input: nums = [2,2,2], target = 2
+
+Output: 3
+Explanation: There are 3 different ways to sum the input numbers to get a sum of 2.
++2 +2 -2 = 2
++2 -2 +2 = 2
+-2 +2 +2 = 2
+
+Constraints:
+
+1 <= nums.length <= 20
+0 <= nums[i] <= 1000
+-1000 <= target <= 1000
+
+*/
+
+
+/*
+idea:
+    recursive call:
+    dfs( from, nums, target)
+    -> current number = nums[from]
+    return dfs(from+1, nums, target + current) 
+            + dfs(from+1, nums, target - current)
+*/
+class Solution {
+private:
+    int dfs(const vector<int>& nums, 
+            int from, int target, 
+            unordered_map<int, int>& memory){
+        if(from==nums.size()) return target == 0;
+        
+        //1 <= nums.length <= 20
+        int key = from + 100 * target;
+        if(memory.count(key)){
+            return memory[key];
+        }
+
+        const auto& current = nums[from];
+        return memory[key] = (
+                dfs(nums, from+1, target+current, memory) 
+             +  dfs(nums, from+1, target-current, memory)); 
+    }
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        unordered_map<int, int> memory;
+        return dfs(nums, 0, target, memory);
+    }
+};
