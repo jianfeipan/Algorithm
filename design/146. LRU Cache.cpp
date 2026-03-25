c25d560f42572f32a8db51a91d948e4308eed7df
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-03-25 12:56:27 +0100

    Update 146. LRU Cache.cpp

--- design/146. LRU Cache.cpp
+++ design/146. LRU Cache.cpp
@@ -52,66 +52,65 @@ idea:
 
 
 */
+/*
+last recent used:
+a map for the key value cache
+    -> unordered_map
+    -> we need to use key to access the list iterator inorder to move it
+a structure with order to keep track the used order
+    -> move node from one position to another -> List
+
+*/
+
 class LRUCache {
-
-    /*
-    fact:
-        1. it's a cache, key value data structur is need:
-            -> unorded_map
-        2. Least recently Used: we need an ordered data structur to track the use order 
-            -> list: remove from middle push to front and remove the tail.   
-    */
 private:
-    using Key = int;
-    using Val = int;
-    using Order = list<pair<Key, Val>>;
-    
-    unordered_map<Key, Order::iterator> d_data;
-    Order d_mostRecent;
-    int d_capacity;
-
+    unordered_map<int, list<pair<int, int>>::iterator> m_data;
+    list<pair<int, int>> m_order; // key value: we need key to evict map
+    int m_capacity;
 public:
-    LRUCache(int capacity) {
-        d_capacity = capacity;
+    LRUCache(int capacity): m_capacity(capacity) {
+
     }
-    
+
     int get(int key) {
-        if(d_data.contains(key)) 
-        {
-            // move this element to the most recent front
-            // which means we need to access it directly
-            auto val = d_data[key]->second;
-            d_mostRecent.erase(d_data[key]);
-            d_mostRecent.push_front({key, val});
-            d_data[key] = d_mostRecent.begin();
-            return val;
+        if(m_data.count(key)){
+            auto hit = m_data[key];
+            //m_order.splice(m_order.begin(), m_order, hit); // slice is change the poitner, more efficient than create copy remove
+            m_order.splice(m_order.begin(), m_order, hit);
+            // m_order.push_front({hit->first, hit->second});
+            // m_order.erase(hit);
+            return m_order.front().second;
         }
-        return -1;
-    }
-    
-    void put(int key, int value) {
-        if(d_data.contains(key))
-        {
-            d_mostRecent.erase(d_data[key]);
-        }
+
         else
-        {
-            if(d_capacity == 0) 
-            {
-                //eviction
-                auto itToEvict = d_mostRecent.rbegin();
-                const auto & keyToEvict = itToEvict->first;
-                d_data.erase(keyToEvict);
-                d_mostRecent.pop_back();
+            return -1;
+    }
+
+    void put(int key, int value) {
+        if(m_data.count(key)){
+            auto hit = m_data[key];
+            m_order.splice(m_order.begin(), m_order, hit); // slice is change the poitner, more efficient than create copy remove
+            hit->second = value;// update
+        }else{
+            if(m_data.size() == m_capacity){
+                // eviction: the last in the order list
+                auto key =  m_order.back().first;
+                m_data.erase(key);
+                m_order.pop_back();
             }
-            else{
-                --d_capacity;
-            }
+            m_order.push_front({key, value});
+            m_data[key] = m_order.begin();
+
         }
-        d_mostRecent.push_front({key,value});
-        d_data[key] = d_mostRecent.begin();
     }
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
 
 
 /**
