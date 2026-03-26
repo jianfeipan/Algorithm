4b5ac10e22bdcf3e175081a2bd2588cb09429fe3
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-03-26 21:15:18 +0100

    Update leet 435. Non-overlapping Intervals.cpp

--- array/leet 435. Non-overlapping Intervals.cpp
+++ array/leet 435. Non-overlapping Intervals.cpp
@@ -1,35 +1,70 @@
 /*
+Given an array of intervals intervals where intervals[i] = [start_i, end_i], return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
 
-Given an array of intervals intervals where intervals[i] = [starti, endi], return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
-
- 
-
+Note: Intervals are non-overlapping even if they have a common point. For example, [1, 3] and [2, 4] are overlapping, but [1, 2] and [2, 3] are non-overlapping.
+--> exclusive
 Example 1:
 
-Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+Input: intervals = [[1,2],[2,4],[1,4]]
+
 Output: 1
-Explanation: [1,3] can be removed and the rest of the intervals are non-overlapping.
+Explanation: After [1,4] is removed, the rest of the intervals are non-overlapping.
+
 Example 2:
 
-Input: intervals = [[1,2],[1,2],[1,2]]
-Output: 2
-Explanation: You need to remove two [1,2] to make the rest of the intervals non-overlapping.
-Example 3:
+Input: intervals = [[1,2],[2,4]]
 
-Input: intervals = [[1,2],[2,3]]
 Output: 0
-Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
- 
- 
-[[1,2],[2,3],[3,4],[1,3]]
-[[1,2],[1,2],[1,2]]
-[[1,2],[2,3]]
-[[-52,31],[-73,-26],[82,97],[-65,-11],[-62,-49],[95,99],[58,95],[-31,49],[66,98],[-63,2],[30,47],[-40,-26]] --> MAY HAVE SEVERAL SOLUTION we need the min one
+*/
+
+
+/*
+fact:
+    1 exclusive: (1, 2) (2, 3) -> no overlapping
+    2 not sorted
+
+idea:
+    1. sort it by start, end
+    2 detect overlapping
+    3 try to remove -> this is tricky
+        the question is minimum intervals to remove
+        -> it's actualy maximum intervals no overlapping
+        -> take 0 -> 
+              |-> dont take 0-> 
+        using a dfs
+          
+
 
 
 */
+class Solution_DP {
+private:
+    int maxNoOverlapping(const vector<vector<int>>& intervals, 
+                        int current, int prev, vector<vector<int>>& memory){
+        if(current == intervals.size()) return 0;
 
-class Solution {
+        auto& cache = memory[current][prev+1];
+        if(cache != -1) return cache;
+
+        // don't take current one
+        int maxCount = maxNoOverlapping(intervals, current+1, prev, memory);
+        if(-1 == prev
+        || intervals[prev][1]<=intervals[current][0]){// no overlapping
+            //current one can be taken
+            maxCount = max(maxCount, 1+maxNoOverlapping(intervals, current+1, current, memory));
+        }
+        return cache = maxCount;
+    }
+
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> memory(intervals.size(), vector<int>(intervals.size(), -1));
+        return intervals.size() - maxNoOverlapping(intervals, 0, -1, memory);
+    }
+};
+
+class Solution_one_pass {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) 
     {
@@ -43,17 +78,18 @@ public:
         
         for(size_t i = 1; i < intervals.size(); ++i)
         {
-            //check overlapping
             if(currnetInterval[1] >intervals[i][0])
-            {
-                toBeDeleted++;
+            {//has overlapping
+                ++toBeDeleted;
                 if(currnetInterval[1] >= intervals[i][1])
-                    currnetInterval = intervals[i];//remove the first one
+                    currnetInterval = intervals[i];//keep the i interval: it ends earlier
                 //remove the first one: currnetInterval stays
             }
-            else
+            else{// no overlapping: we just move to next interval
                 currnetInterval = intervals[i];
+            }
         }
         return toBeDeleted;
     }
 };
+
