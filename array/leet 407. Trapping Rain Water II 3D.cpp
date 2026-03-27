50d61a2c25383990a65daacea7cf3e1f308af647
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-03-27 21:49:01 +0100

    Update leet 407. Trapping Rain Water II 3D.cpp

--- array/leet 407. Trapping Rain Water II 3D.cpp
+++ array/leet 407. Trapping Rain Water II 3D.cpp
@@ -1,36 +1,50 @@
-int[][] dirs = new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    
-public int trapRainWater(int[][] heightMap) {
-    int m = heightMap.length;
-    int n = (m == 0 ? 0 : heightMap[0].length);
-    int res = 0;
+
+class Solution {
+public:
+    int trapRainWater(vector<vector<int>>& heightMap) {
+        if (heightMap.empty() || heightMap[0].empty()) return 0;
+
+        const int  R = heightMap.size();
+        const int C = heightMap[0].size();
+        if (R < 3 || C < 3) return 0;
         
-    PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
-    boolean[][] visited = new boolean[m][n];
-        
-    for (int i = 0; i < m; i++) {
-        pq.offer(new int[] {i, 0, heightMap[i][0]});
-        pq.offer(new int[] {i, n - 1, heightMap[i][n - 1]});
-        visited[i][0] = visited[i][n - 1] = true;
-    }
-        
-    for (int j = 1; j < n - 1; j++) {
-        pq.offer(new int[] {0, j, heightMap[0][j]});
-        pq.offer(new int[] {m - 1, j, heightMap[m - 1][j]});
-        visited[0][j] = visited[m - 1][j] = true;
-    }
-        
-    while (!pq.isEmpty()) {
-        int[] cell = pq.poll();
-        	
-        for (int[] d : dirs) {
-            int i = cell[0] + d[0], j = cell[1] + d[1];
-            if (i < 0 || i >= m || j < 0 || j >= n || visited[i][j]) continue;
-            res += Math.max(0, cell[2] - heightMap[i][j]);
-            pq.offer(new int[] {i, j, Math.max(heightMap[i][j], cell[2])});
-            visited[i][j] = true;
+        using T = tuple<int,int,int>;
+        vector<vector<bool>> visited(R, vector<bool>(C, false));
+
+        // push all boundry cells to the minHeap 
+        priority_queue<T, vector<T>, greater<T>> boundries;
+        for (int i = 0; i < R; ++i) {
+            boundries.emplace(heightMap[i][0], i, 0); visited[i][0] = true;
+            boundries.emplace(heightMap[i][C-1], i, C-1); visited[i][C-1] = true;
         }
+        for (int j = 0; j < C; ++j) {
+            if (!visited[0][j]) { boundries.emplace(heightMap[0][j], 0, j); visited[0][j] = true; }
+            if (!visited[R-1][j]) { boundries.emplace(heightMap[R-1][j], R-1, j); visited[R-1][j] = true; }
+        }
+
+
+
+        int res = 0;
+        int dirs[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        // traversal all unvisited cells, if the new one is smaller the current one
+        while (!boundries.empty()) {
+            auto [h, r, c] = boundries.top(); boundries.pop();
+            for (auto &d : dirs) { // visit 4 direction
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if (nr < 0 || nr >= R || nc < 0 || nc >= C || visited[nr][nc]) continue;
+                visited[nr][nc] = true;
+
+                int nh = heightMap[nr][nc];
+
+                if (nh < h) 
+                    res += (h - nh);
+                //nh will trap water, then we deal nh as h to be a part of the bountry in the queue.
+                boundries.emplace(max(nh, h), nr, nc);
+            }
+        }
+        return res;
     }
-        
-    return res;
-}
+};
