d583c0dc4cf0a4b38080dd635519dbe6c0692f2a
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-03-27 16:04:07 +0100

    Create hamming_weight.cpp

---
+++ math/hamming_weight.cpp
@@ -0,0 +1,15 @@
+/*
+how many 1 in the binary number n
+*/
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        while(n){
+            if(n&1)  ++count;
+            n>>=1;
+        }
+        return count;
+    }
+};
