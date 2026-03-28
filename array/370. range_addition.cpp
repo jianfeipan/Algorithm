41e33da23b6dd995dc9d3384aa352866180e54af
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-03-28 19:02:13 +0100

    Update 370. range_addition.cpp

--- array/370. range_addition.cpp
+++ array/370. range_addition.cpp
@@ -2,11 +2,11 @@
 #include <vector>
 #include <cassert>
 /*
-You are given an integer length 
+You are given an integer length
 (the size of an array initialized to all zeros) and a list of updates.
 Each update is a triplet: [start_index, end_index, increment].
 
-Example 
+Example
 Input: length = 5, updates = [[1, 3, 2]]
 
 Initialize: [0, 0, 0, 0, 0]
@@ -20,7 +20,7 @@ Apply Update: [0, 2, 2, 2, 0]
 length = N; modifications K
 Brute Force: O(K*N)
 
-  
+
 
 */
 using namespace std;
@@ -42,7 +42,7 @@ public:
          {0, 2, -2}   -2-2-2
                      [-2 0 3 5 3]
 
-         all ranges are [from, to], we can transform them to leftSum[l-1] - leftSum[r]      
+         all ranges are [from, to], we can transform them to leftSum[l-1] - leftSum[r]
           {1, 3, 2} -> {0, 3, 2} + {0, 0, -2}
           {2, 4, 3} -> {0, 4, 3} + {0, 1, -3}
           {0, 2,-2} -> {0, 2,-2}
@@ -50,29 +50,25 @@ public:
           so we have, they are piece-wise shifters: 0 shift -1, then piece-wise, then 1 shift -3...
           we place these shifters on the [0 0 0 00 0 0 0 0 0 ]
           and we do accumulate from left, it ill be the final result
-                     
-          
+
+
         */
-        
+
         for (const auto& update : updates) {
             const auto& left = update[0];
             const auto& right = update[1];
             const auto& increment= update[2];
-            
-            /* 
-              transform range to "left side jumps" 
-                right index jump val
-                left-1 index jump -val
+
+            /*
+              transform range to  jump start from index
             */
-            res[right] += val;
-          
-            if (left>0) {
-                res[left-1] -= val;
-            }
+            res[left] += increment;
+            if(right<length-1)
+                res[right+1] -= increment;
         }
         // we do accumulate from right, piece-wise the jumps
-        for(int i=res.size()-2; i>=0; --i) res[i]+=res[i+1];
-        
+        for(int i=1; i<length; ++i) res[i]+=res[i-1];
+
         return res;
     }
 };
@@ -94,7 +90,7 @@ int main() {
     vector<vector<int>> updates1 = {{1, 3, 2}, {2, 4, 3}, {0, 2, -2}};
     vector<int> expected1 = {-2, 0, 3, 5, 3};
     vector<int> result1 = sol.getModifiedArray(len1, updates1);
-    
+
     cout << "Test Case 1: ";
     printArray(result1);
     assert(result1 == expected1);
@@ -104,7 +100,7 @@ int main() {
     vector<vector<int>> updates2 = {{2, 4, 6}, {5, 6, 8}, {1, 9, -4}};
     vector<int> expected2 = {0, -4, 2, 2, 2, 4, 4, -4, -4, -4};
     vector<int> result2 = sol.getModifiedArray(len2, updates2);
-    
+
     cout << "Test Case 2: ";
     printArray(result2);
     assert(result2 == expected2);
