785c2d22d85b68a4645b044d768cbda5f21e873f
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-03-28 16:55:08 +0100

    Update 2771. Longest Non-decreasing Subarray From Two Arrays.cpp

--- DP/2771. Longest Non-decreasing Subarray From Two Arrays.cpp
+++ DP/2771. Longest Non-decreasing Subarray From Two Arrays.cpp
@@ -99,6 +99,22 @@ public:
         //  0 nums1      1
         //  1 nums2      1
 
+
+        /*
+        dp1 means the maximum step ending with A[i].
+        dp2 means the maximum step ending with B[i].
+
+        Then cosider these 4 transitions:
+        t11 for A[i - 1] -> A[i]
+        t12 for A[i - 1] -> B[i]
+        t21 for B[i - 1] -> A[i]
+        t22 for B[i - 1] -> B[i]
+
+        We update
+        dp1 = max(t11, t21)
+        dp2 = max(t12, t22)
+        res = max(res, dp1, dp2)*/
+
         int res = 1, dp1 = 1, dp2 = 1, n = A.size(), t11, t12, t21, t22;
         for (int i = 1; i < n; ++i) {
             t11 = A[i - 1] <= A[i] ? dp1 + 1 : 1;
