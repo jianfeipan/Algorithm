71343ce19d0054b4d519a3bd305b6ea7bf92a3d6
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-03-29 12:04:38 +0200

    Update minimum_window_substr.cpp

--- array/sliding_window/minimum_window_substr.cpp
+++ array/sliding_window/minimum_window_substr.cpp
@@ -1,42 +1,72 @@
+/*
+window in s has all letters in t including duplications:
+    - not a exact match: window can have exact number of letter in c or more!
+
+
+Input: 
+     l    r
+s = "OUZODYXAXZV", 
+t = "XYZX"
+window: 
+    match: ++l : when its a match update minLen
+    else: ++r 
+macth: 
+    all chars and duplications-> use a data strucutre to count
+    unorderd_map<char, int> 
+    uppercase and lowercase? is case sensitive? 
+
+s="ADOBECODEBANC"
+t="ABC"
+
+*/
+
 class Solution {
 public:
     string minWindow(string s, string t) {
-        // every char in t is present in the substr of s
-        if (t.empty()) return "";
+        if(s.size() < t.size()) return "";
 
-        unordered_map<char, int> countT, window;
-        for (char c : t) {
-            countT[c]++;
-        }
+        unordered_map<char, int> target;
+        for(auto c : t) ++target[c];
 
-        int have = 0;
+        int minLen=INT_MAX;
+        int minLenStart=-1;
+        int match = 0;
 
-        int l=0; 
-        int r=0;
-        int minLen = INT_MAX;
-        int minL = -1;
+        int l=0, r=0;
+        unordered_map<char, int> current;
         while(r<s.size()){
-            char c = s[r];
-            window[c]++;
-            if (countT.contains(c) && window[c] == countT[c]) ++have;
-
-            while(have == countT.size()){// 
-                auto len = r-l+1;
+            const auto& c = s[r];
+           
+            if(target.contains(c)){
+                ++current[c];
+                if(target[c] == current[c]){
+                    ++match;
+                }
+                // if it's exact match: 
+                // else if(target[c]+1 == current[c]){
+                //     --match;
+                // }
+            }
+            
+            // match: shrink the left (++l)
+            while(match == target.size()){
+                const auto len = r-l+1;
                 if(len < minLen){
                     minLen = len;
-                    minL = l;
+                    minLenStart = l;
                 }
                 
-                --window[s[l]];
-                if (countT.contains(s[l]) && window[s[l]] < countT[s[l]]) --have;
-                
+                if(current.contains(s[l])){
+                    --current[s[l]];
+                    if(current[s[l]] == target[s[l]]-1) --match;
+                    // exact match : if(current[s[l]] == target[s[l]]-1) --match;
+                }
                 ++l;
             }
 
             ++r;
         }
-        
-        return minL == -1 ? "" : s.substr(minL, minLen);
-        
+
+        return minLenStart==-1? "" : s.substr(minLenStart, minLen);
     }
 };
