0a1cb29c183a62ede6f83dbb8acbf6d972f17c5a
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-04-02 13:57:06 +0200

    Create 2484. Count Palindromic Subsequences_prefix_map.cpp

---
+++ array/2484. Count Palindromic Subsequences_prefix_map.cpp
@@ -0,0 +1,71 @@
+/*
+Given a string of digits s, return the number of palindromic subsequences of s having length 5. Since the answer may be very large, return it modulo 109 + 7.
+
+Note:
+
+A string is palindromic if it reads the same forward and backward.
+A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters.
+ 
+
+Example 1:
+
+Input: s = "103301"
+Output: 2
+Explanation: 
+There are 6 possible subsequences of length 5: "10330","10331","10301","10301","13301","03301". 
+Two of them (both equal to "10301") are palindromic.
+Example 2:
+
+Input: s = "0000000"
+Output: 21
+Explanation: All 21 subsequences are "00000", which is palindromic.
+Example 3:
+
+Input: s = "9999900000"
+Output: 2
+Explanation: The only two palindromic subsequences are "99999" and "00000".
+
+*/
+
+#include <array>
+class Solution {
+public:
+    int countPalindromes(string s) {
+        int mod = 1e9 + 7;
+        const auto n=s.size();
+        vector<array<array<int, 10>, 10>> before(n);
+        {// before i, counts of A_B
+            array<int, 10> count;
+            ++count[s[0] - '0'];
+            for(int i=1; i<n; ++i){
+                int current = s[i] - '0';
+                before[i] = before[i-1];
+                for(int a=0; a<10;++a)
+                    before[i][a][current] += count[a];// += how Many A so far
+                ++count[current];
+            }
+        }
+        vector<array<array<int, 10>, 10>> after(n);
+
+        {// after i, counts of A_B: from right to left
+            array<int, 10> count;
+            ++count[s[n-1] - '0'];
+            for(int i=n-2; i>=0; --i){
+                int current = s[i] - '0';
+                after[i] = after[i+1];
+                for(int a=0; a<10;++a)
+                    after[i][a][current] += count[a];// += how Many A so far
+                ++count[current];
+            }
+        }
+        int ans=0;
+        for (int i = 2; i <= n - 2; i++){
+            for (int a = 0; a < 10; a++)
+                for (int b = 0; b < 10; b++)
+                    ans = (ans + 1LL * before[i - 1][a][b] * after[i + 1][a][b]) % mod;
+        }
+        return ans;
+            
+
+    }
+};
