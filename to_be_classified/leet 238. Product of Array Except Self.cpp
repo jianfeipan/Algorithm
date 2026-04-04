32c9dedb4bc7a3d1b73a5765a2a91415075676bd
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-04-04 10:24:58 +0200

    Refactor productExceptSelf function for clarity

--- to_be_classified/leet 238. Product of Array Except Self.cpp
+++ to_be_classified/leet 238. Product of Array Except Self.cpp
@@ -3,17 +3,16 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         vector<int> prefixProduct = vector<int>(nums.size(), 1);
         //prefixProduct[i] = nums[0]*...nums[i-1]
-        {//cumulate from left   
-            for(int i = 1; i<prefixProduct.size(); ++i)
-                prefixProduct[i] = prefixProduct[i-1] * nums[i-1];
-        }
+        //cumulate from left   
+        for(int i = 1; i<prefixProduct.size(); ++i)
+            prefixProduct[i] = prefixProduct[i-1] * nums[i-1];
+        
 
         vector<int> suffixProduct = vector<int>(nums.size(), 1);
-        //prefixProduct[i] = nums[i+1]*...nums[n-1]
-        {//cumulate from right   
-            for(int i = suffixProduct.size() - 2; i>=0; --i)
-                suffixProduct[i] = suffixProduct[i+1] * nums[i+1];
-        }
+        //suffixProduct[i] = nums[i+1]*...nums[n-1]
+        //cumulate from right   
+        for(int i = suffixProduct.size() - 2; i>=0; --i)
+            suffixProduct[i] = suffixProduct[i+1] * nums[i+1];
 
         vector<int> products(nums.size(), 1);
         for(int i=0; i<products.size(); ++i) products[i]=prefixProduct[i] * suffixProduct[i];
