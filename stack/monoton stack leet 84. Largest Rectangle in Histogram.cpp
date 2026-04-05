ca5a7fff07abb26290ea112b0347ee7b0d2532be
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-04-05 11:57:40 +0200

    Update monoton stack leet 84. Largest Rectangle in Histogram.cpp

--- stack/monoton stack leet 84. Largest Rectangle in Histogram.cpp
+++ stack/monoton stack leet 84. Largest Rectangle in Histogram.cpp
@@ -42,28 +42,52 @@ idea:
 */
 
 class Solution {
+
+
+/*                0 1 2 3 4 5
+Input: heights = [7,1,7,2,2,4]
+
+area = minH * (r-l+1)
+r   minH area
+0    7     7
+1    1     2
+2    1     3 < 7
+
+area depends: current height, and how far this height can extend to left
+                                  -> monoton stack: keep track heigher in front (decreasing)
+
+r   cur   area        stack
+0    7     7           7
+1    1     2           7>1 : the last element of 7 cannot extend to right, so the max for this 7, is on left, and we will pop  them, becasue the current one will make those heights not usable by pillars on the right of current
+2    7     ?           1,7
+3    2     7           1,2
+...                  1  3  4  5
+5    4              [1, 2, 2, 4]
+6*               max(4*(6-4-1) ,  2*(6-3-1), 2*(6-1-1))
+* we need the index in the stack to calculate the width
+
+*/
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        int maxArea = 0;
-        stack<int> increasing;
+
+        int largest=0;
+        heights.push_back(0);// for the last one to be out of stack
         
-        for(int i=0; i<=n; ++i){
-            while(!increasing.empty() 
-                && (heights[increasing.top()] >= heights[i]
-                    || i == n)){
-                auto lastBigger = heights[increasing.top()];
+        stack<int> increasing;
+        for(int r=0; r<heights.size(); ++r){
+            while(!increasing.empty() && heights[increasing.top()]>heights[r]){
+                // the top reaches the end to r, it can only extend to left
+                // to left, it stops next top, next top is smaller than this top
+                auto height = heights[increasing.top()];
                 increasing.pop();
-                // witdh to previous smaller
-                auto width = increasing.empty() ? i : i - increasing.top() - 1;
-                maxArea = max(maxArea, width*lastBigger);
+                auto leftBoundry = increasing.empty() ? -1 : increasing.top();
+                auto width = r - 1 - leftBoundry;
+                largest = max(largest, height*width);
             }
-            increasing.push(i);
+            increasing.push(r);
         }
-       
-
-
-        return maxArea;
+        return largest;
 
     }
 };
+
