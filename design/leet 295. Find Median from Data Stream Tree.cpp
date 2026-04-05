c76cea4b5683e969f5bc33b9190ebb87e8261d00
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-04-05 15:36:26 +0200

    Update leet 295. Find Median from Data Stream Tree.cpp

--- design/leet 295. Find Median from Data Stream Tree.cpp
+++ design/leet 295. Find Median from Data Stream Tree.cpp
@@ -1,44 +1,32 @@
 class MedianFinder {
+private:
+    // from small to big
+    priority_queue<int, vector<int>, less<int>> top;
+    priority_queue<int, vector<int>, greater<int>> bottom;
+    /*
+        top         bottom
+    [ 1 2 3     |    5 6 7 8    ]
+    */
 public:
-    MedianFinder() :data(),mid(),left(0), right(0)
-    {
+    MedianFinder() {
         
     }
     
     void addNum(int num) {
-        data.insert(num);
-        
-        if(data.size()==1) 
-        {
-            mid = data.begin();
-        }    
-        else
-        {
-            if(num<*mid) ++left;
-            else ++right; 
-            
-            if(left>right && left - right==1)  --mid,--left,++right;
-            else if(right>left && right - left==2) ++mid, --right,++left;
+        if(bottom.empty() || num >= bottom.top()) bottom.push(num);
+        else top.push(num);
+
+        if(top.size()+1 < bottom.size()){
+            top.push(bottom.top());
+            bottom.pop();
+        }else if(top.size() > bottom.size()){
+            bottom.push(top.top());
+            top.pop();
         }
-        //cout<<*mid<<endl;
     }
     
     double findMedian() {
-        if(data.size()%2 ==1)
-            return *mid;
-        else
-            return (*mid + *next(mid)) * 0.5;
+        if(top.size()  ==  bottom.size()) return (top.top() + bottom.top()) / 2.0;
+        else return bottom.top();
     }
-    
-    multiset<int> data;
-    multiset<int>::iterator mid;
-    int left;
-    int right;
 };
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
- */
