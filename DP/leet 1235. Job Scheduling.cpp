5404bd293b25aee37ddbc1eab52b22452faecb70
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-04-10 11:28:06 +0200

    Update leet 1235. Job Scheduling.cpp

--- DP/leet 1235. Job Scheduling.cpp
+++ DP/leet 1235. Job Scheduling.cpp
@@ -1,6 +1,4 @@
-class Solution {
-
-   /*
+  /*
    We have n jobs, where every job is scheduled to be done from startTime[i] to endTime[i], obtaining a profit of profit[i].
 
 You're given the startTime, endTime and profit arrays, return the maximum profit you can take such that there are no two jobs in the subset with overlapping time range.
@@ -19,78 +17,49 @@ If you choose a job that ends at time X you will be able to start another job th
 [5,6,4]
 
     */
-public:
-   int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit)
-   {
-      return jobScheduling_divide_conquer_with_memory(startTime, endTime, profit);
-   }
-    
-    
-    int jobScheduling_DP_tablution(vector<int>& startTime, vector<int>& endTime, vector<int>& profit)
-    {
-        int n = startTime.size();
-        vector<vector<int>> jobs;jobs.reserve(n);
-        for (int i = 0; i < n; ++i)  jobs.push_back({endTime[i], startTime[i], profit[i]});
-        
-        sort(jobs.begin(), jobs.end());
-        
-        map<int, int> dp = {{0, 0}};
-        
-        for (auto& job : jobs) {
-            int cur = prev(dp.upper_bound(job[1]))->second + job[2];
-            if (cur > dp.rbegin()->second)
-                dp[job[0]] = cur;
-        }
-        return dp.rbegin()->second;
-    
-    }
-
-
-   int jobScheduling_divide_conquer_with_memory(vector<int>& startTime, vector<int>& endTime, vector<int>& profit)
-   {
-      vector<tuple<int, int, int>> jobs; jobs.reserve(startTime.size());
-
-      for (size_t i = 0; i < startTime.size(); ++i) jobs.emplace_back(startTime[i], endTime[i], profit[i]);
-
-      sort(jobs.begin(), jobs.end(),
-         [](const tuple<int, int, int> & left, const tuple<int, int, int> & right) {return get<0>(left) < get<0>(right); });
-
-      unordered_map<int, int> memory;
-
-      return jobScheduling_recusive_with_memory(jobs, 0, memory);
-   }
-
-
-
+class Solution {
 private:
-    int jobScheduling_recusive_with_memory(const vector<tuple<int, int, int>>& jobs, int currentJob, unordered_map<int, int> memory)
-    {        
-        if(currentJob == jobs.size()) return 0;
-        
-        if (memory.count(currentJob)) return memory[currentJob];
-        
-        const int maxProfitIfNotTakeCurrent = jobScheduling_recusive_with_memory(jobs, currentJob+1, memory);
+    int jobSchedulingFrom(const vector<vector<int>>& tasks, int i,
+                      vector<int>& dp) {
+        if (i >= tasks.size()) return 0;
+        if (dp[i] != -1) return dp[i];
 
-        int maxProfitIfTakeCurrent = get<2>(jobs[currentJob]) ;
+        // skip task i
+        int res = jobSchedulingFrom(tasks, i + 1, dp);
 
-        int nextJobIfTakeCurrent = findNext(jobs, currentJob);
-        if(nextJobIfTakeCurrent!=-1) maxProfitIfTakeCurrent+=jobScheduling_recusive_with_memory(jobs, nextJobIfTakeCurrent, memory);
-        
-        const int maxProfit = max(maxProfitIfTakeCurrent, maxProfitIfNotTakeCurrent);
+        // take task i → binary search for next non-overlapping task
+        int nextIdx = lower_bound(tasks.begin() + i + 1, tasks.end(), tasks[i][1],
+            [](const vector<int>& t, int endTime) { return t[0] < endTime; }
+        ) - tasks.begin();
 
-        memory[currentJob] = maxProfit;
-        
-        return maxProfit;
+        res = max(res, tasks[i][2] + jobSchedulingFrom(tasks, nextIdx, dp));
+        return dp[i] = res;
     }
-    
-    
-    int findNext(const vector<tuple<int, int, int>>& jobs, int currentJob)
-    {
-        auto currentEnd = get<1>(jobs[currentJob]);
-        for(int i = currentJob+1; i<jobs.size(); ++i)
-        {
-            if(get<0>(jobs[i]) >= currentEnd) return i;
+
+public:
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        // DP: 
+        vector<int> dp(startTime.size(), -1);
+
+        vector<vector<int>> sortedTasks; sortedTasks.reserve(startTime.size());
+        for(int i=0; i<startTime.size(); ++i){
+            sortedTasks.push_back({startTime[i], endTime[i], profit[i]});
         }
-        return -1;
+
+        sort(sortedTasks.begin(), sortedTasks.end());
+
+        return jobSchedulingFrom(sortedTasks, 0, dp);
     }
+
 };
+
+
+/*
+ 0   1   2   4
+[1,  2,  3,  3]
+[3,  4 , 5,  6]
+[50,10, 40, 70]
+
+
+
+*/
