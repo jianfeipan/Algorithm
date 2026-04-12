82af7b1b6da410387e8931bcf9508377eb133ea7
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-04-12 21:57:43 +0200

    Update 229.count_majority_3.cpp

--- math/229.count_majority_3.cpp
+++ math/229.count_majority_3.cpp
@@ -14,15 +14,18 @@ public:
         int count2 = 0;
         for(auto num : nums){
             // new major
-            if(count1 == 0) major1 = num;
-            else if(count2 == 0) major2 = num;
+            
 
             // count
             if(num == major1) ++count1;
             else if(num == major2) ++count2;
+            else if(count1 == 0) {major1 = num; count1=1;}
+            else if(count2 == 0) {major2 = num; count2=1;}
             else{
                 --count1;
                 --count2;
+                /*
+                当我们同时减少两个计数器时，本质上是丢弃了三个互不相同的数：候选人 A 的一个实例。候选人 B 的一个实例。当前遇到的第三个不同的数。因为我们每次丢弃的是 3 个不同的数，这种“三方抵消”在长度为 $n$ 的数组中最多只能发生 $n/3$ 次。如果一个元素的出现次数真的超过了 $n/3$，它在经历了所有的抵消之后，一定会有剩余。虽然它的最终计数不一定是它的实际出现次数，但它一定会留在 major1 或 major2 中。*/
             }
         }
 
