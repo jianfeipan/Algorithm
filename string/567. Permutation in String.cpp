eb84da88b0b0c1c4314e2a9fa19730e359a02b71
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-04-30 15:25:08 +0200

    Refactor checkInclusion method for clarity and efficiency

--- string/567. Permutation in String.cpp
+++ string/567. Permutation in String.cpp
@@ -15,112 +15,59 @@ Input: s1 = "ab", s2 = "eidboaoo"
 Output: false
 
 */
-
 class Solution {
-/*
-fact:
-    s1 is the shorter one, if not, falase
-    substring, means in a window, letters have the same histogram as S1
-idea:
-    use a map as histogram of s1 letters and a sliding window to scan s2
-
-*/
-
-
-
-
-
-
 public:
-//better solution:
- bool checkInclusion(string s1, string s2) {
-        //s2 's substring,  matches exactly all s1 letters
-        // the key here is the substring must have the len(s1), so our window is fixed !! fixed !!!
-        // if we have, means substring should have the same hinstogram: a:1, b:1, c:1...
-        if(s2.size()<s1.size()) return false;
-    
-        // the histogram (Both strings only contain lowercase letters.)
-        int s1Count[26]={0};
-        int s2Count[26]={0};
-        for(int i = 0; i<s1.size(); ++i){
-            ++s1Count[s1[i]-'a'];
-            ++s2Count[s2[i]-'a'];
-        }
-
-        int matches = 0;
-        for(int i = 0; i<26; ++i){
-            if(s1Count[i] == s2Count[i]) ++matches;
-        }
-        
-        int left = 0; 
-        int right = s1.size()-1;
-        while(right<s2.size()){
-            if(matches == 26) return true;
-
-            ++right;
-            ++s2Count[s2[right]-'a'];
-            if(s2Count[s2[right]-'a'] == s1Count[s2[right]-'a']) //from not match to match
-                ++matches;
-            else if(s2Count[s2[right]-'a'] - 1 == s1Count[s2[right]-'a']) //from match to not match
-                --matches;
-            
-            --s2Count[s2[left]-'a'];
-            if(s2Count[s2[left]-'a'] == s1Count[s2[left]-'a']) 
-                ++matches;
-            else if(s2Count[s2[left]-'a'] + 1 == s1Count[s2[left]-'a']) 
-                --matches;
-            ++left;
-        }
-        return false;
-    }
-
-
-
-
-
-
     bool checkInclusion(string s1, string s2) {
         if(s1.size() > s2.size()) return false;
 
+        const auto& m = s1.size();
+        const auto& n = s2.size(); 
+
         unordered_map<char, int> target;
-        for(char c : s1) ++target[c]; 
+        for(auto c : s1) ++target[c];
 
-        int windwLenth = s1.size();
-
-        int first = 0; 
-        int last = 0;
-        while(!target.empty() && first <= last && last<s2.size()){
-            char current = s2[last];
-            auto it = target.find(current);
-            if(it!=target.end())
-            {
-                --(it->second);//last chat get in the window
-                if(it->second == 0)
-                {
-                   target.erase(it);     
-                }
-
-                ++last;
+        unordered_map<char, int> window;
+        /*
+        s1 = "abc", 3 
+        s2 = "lecabee" 7
+        0123456
+            r        
+        lecabee
+        */
+        int match=0;
+        int r=0;
+        //build window
+        for(; r<m; ++r){
+            auto current = s2[r];
+            if(target.count(current)){
+                ++window[current];
+                if(window[current] == target[current]) ++match;   
+                else if(window[current] == target[current]+1) --match;
             }
-            else
-            {
-                if(first == last) 
-                {
-                    ++last;++first;
-                }
-                else
-                {
-                    ++target[s2[first]]; //first get out
-                    ++first;
-                }
-
-            }
-
-            //cout<< first << "-" <<last<<endl;
-
         }
+        if(match == target.size()) return true;
+        
+        // slide fixed window
+        int l=0;
+        for(; r<n; ++r,++l){
+            auto left = s2[l];
+            if(target.count(left)){
+                --window[left];
+                if(window[left] == target[left]) ++match;
+                else if(window[left] == target[left]-1) --match;
+            }
 
-        return target.empty();
-
+            auto right = s2[r];
+            if(target.count(right)){
+                ++window[right];
+                if(window[right] == target[right]) ++match;
+                else if(window[right] == target[right]+1)--match;
+            }
+            if(match == target.size()) return true;
+        }
+        
+        
+        return false;
     }
 };
+
