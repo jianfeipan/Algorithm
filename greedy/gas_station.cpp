6d4d49825f37566ae4336393cde1a39e1ecf1e94
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-05-05 21:56:24 +0200

    Update gas_station.cpp

--- greedy/gas_station.cpp
+++ greedy/gas_station.cpp
@@ -26,6 +26,47 @@ public:
         return min_tank_index == n-1 ? 0 : min_tank_index + 1;
     }
 };
+
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        /*
+        try to find a start that can reach end,
+
+        initialize  end to 0, 
+                    start to n-1,
+        try to check from current start if we can have a tank negative:
+            if tank<0 try a earliler start start--
+            if tank=> try go further :  end++
+
+            when start meet end: we finished
+                if tank < 0 : cannot do it.
+                if tank < 
+        */
+        const auto& n = gas.size();
+        int start = n-1;
+        int end = 0;
+        int tank = gas[start] - cost[start];
+
+        while(start>end){
+            if(tank<0){
+                // start is not possible, in order to be able to reach start, 
+                // we need to start earlier to get more gas in tank
+                --start;
+                tank += gas[start] - cost[start];
+            }else{
+                // this start is possible, wich means we can go further
+                // let's extend end and see if we can go one more step
+                ++end;
+                tank += gas[end] - cost[end]
+            }
+        }
+       
+        // when start reaches end, if tank < 0; we don't find a way
+        return tank<0? -1 : start;
+    }
+};
 /*
 There are n gas stations along a circular route. You are given two integer arrays gas and cost where:
 
