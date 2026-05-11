c273baf9c176a8088fe767264b4adfd7d79fa424
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-05-11 16:34:15 +0200

    Update Subsets.78.cpp

--- backtracking/combination_permutation_subset/Subsets.78.cpp
+++ backtracking/combination_permutation_subset/Subsets.78.cpp
@@ -24,3 +24,30 @@ public:
     }
 };
 
+class Solution_with_duplications {
+
+private:
+    void subset_(const vector<int>& nums, int from, vector<int>& current, vector<vector<int>>&res){
+        res.push_back(current); // Every node in the tree is a valid subset
+
+        for (int i = from; i < nums.size(); ++i) {
+            // CRITICAL: Skip duplicates. 
+            // If this is not the first time we are picking an element for this 'slot'
+            // and it's the same as the previous element, skip it.
+            if (i > from && nums[i] == nums[i-1]) continue;
+
+            current.push_back(nums[i]);
+            subset_(nums, i + 1, current, res);
+            current.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        vector<int> current; current.reserve(nums.size());
+        subset_(nums, 0, current, res);
+        return res;
+    }
+};
+
