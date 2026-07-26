f0cb4b2802f44219e5a77c1c47c24ab1ea70b526
jianfeipan <jianfei.pan.tb@gmail.com>
2026-07-26 19:10:43 +0200

    refactoring

--- cpp/concurrency/1_from_mutex_to_serial_queue.cpp
+++ cpp/concurrency/1_from_mutex_to_serial_queue.cpp
@@ -23,15 +23,13 @@ Lock based model:
  */
 
 class registry {
-    mutex mtx_;
     unordered_map<string, string> data_;
+    mutex mtx_;
 public:
     optional<string> get(const string& key) {
         lock_guard<mutex> g(mtx_);
-
         auto it = data_.find(key);
         if (it != data_.end()) return it->second;
-
         return nullopt;
     }
 
@@ -52,61 +50,72 @@ A mutex serializes access to a resource:
 
 // serial queue: a single consumer wrapper of a notification queue
 class serial_queue {
-    deque<function<void()>> q_;
-    mutex                   mtx_;
-    condition_variable      cv_;
-    bool                    done_ {false};
-    thread                  worker_;
+    using Task = function<void()>;
+    deque<Task> q_;
+    thread worker_;
+    mutex mtx_;
+    condition_variable cv_;
+    bool stop_{false};
 
     void run() {
-        deque<function<void()>> cache; 
-        while (true) {
+        deque<Task> local_cache;
+        while(true) {
             {
                 unique_lock<mutex> lock(mtx_);
-                cv_.wait(lock, [&]{return done_ || !q_.empty(); });
-                if (q_.empty() && done_) return;
-                cache.swap(q_);
+                cv_.wait(lock, [this]{ return !q_.empty() || stop_; }); 
+                if (q_.empty() && stop_) return;
+                swap(q_, local_cache);
             }
-            
-            while (!cache.empty()) { // drai the queue
-                cache.front()();
-                cache.pop_front();
+
+            while(!local_cache.empty()) { // single thread: run all task
+                move(local_cache.front())();
+                local_cache.pop_front();
             }
         }
     }
-
 public:
-    serial_queue() : worker_([this]{ run(); }) { }
+    serial_queue() : worker_([this]{ run(); }){}
+
     ~serial_queue() {
         {
             lock_guard<mutex> g(mtx_);
-            done_ = true;
+            stop_ = true;
         }
         cv_.notify_all();
-        worker_.join();
+        if (worker_.joinable()) worker_.join();
     }
 
     serial_queue(const serial_queue&) = delete;
     serial_queue& operator=(const serial_queue&) = delete;
 
-    template<typename F, typename ...Args>
-    auto async(F&& f, Args&& ...args) -> future<invoke_result_t<F, Args...>>{
-        using return_type = invoke_result_t<F, Args...>;
+    void async(Task&& task) {
+        lock_guard<mutex> g(mtx_);
+        q_.emplace_back(move(task));
+        cv_.notify_one();
+    }
 
-        auto p_task = make_shared<packaged_task<return_type()>>(
-            [
-                fn = forward<F>(f), 
-                tup = make_tuple(forward<Args>(args)...)
-            ]() mutable -> return_type {
-                return apply(move(fn), move(tup));
-            }
+    // more powerful async allowing return and input
+    template <typename F, typename... Args>
+    auto async(F&& f, Args&&... args) {
+        using return_type = std::invoke_result_t<F, Args...>;
+
+        // Bind function and args directly inside a simple lambda capture
+        // auto bound_fn = [fn = std::forward<F>(f), 
+        //                 ...args = std::forward<Args>(args)]() mutable {
+        //     return std::invoke(std::move(fn), std::move(args)...);
+        // };
+
+        // auto task = std::make_shared<std::packaged_task<return_type()>>(std::move(bound_fn));
+        // std::bind_front automatically binds and stores the args cleanly
+        auto task = std::make_shared<std::packaged_task<return_type()>>(
+            std::bind_front(std::forward<F>(f), std::forward<Args>(args)...)
         );
-        // apply+tup > invoke(f, args...) > bind : move only object, perfect forwarding
+        
+        auto future = task->get_future();
 
-        auto future  = p_task->get_future();
         {
-            lock_guard<mutex> lock(mtx_);
-            q_.emplace_back([p_task] { (*p_task)(); });
+            std::lock_guard<std::mutex> lock(mtx_);
+            q_.emplace_back([task] { (*task)(); });
         }
         cv_.notify_one();
         return future;
@@ -115,26 +124,37 @@ public:
 
 // Any mutex based lock and be transform to a serial queue
 class registry_serial {
-    serial_queue sq_;
-    unordered_map<string, string> data_;
+    using map_t = unordered_map<string, string>;
+    shared_ptr<map_t> data_ = make_shared<map_t>();
+    serial_queue q_;
+
 public:
     future<optional<string>> get(const string& key) {
-        return sq_.async([this, key]->optional<string> {
-            auto it = data_.find(key);
-            if(it!=data_.end()) return it->second;
-            return nullopt;
-        });
+        using return_type = optional<string>;
+        auto task = make_shared<packaged_task<return_type()>>(
+            [data_ = data_, key] -> return_type {
+                auto it = data_->find(key);
+                if(it != data_->end()) return it->second;
+                return nullopt;
+            }
+        );
+
+        auto future = task->get_future();
+
+        q_.async([task]{ (*task)(); });
+        return future;
     }
 
     void set(const string& key, const string& val) {
-        sq_.async([this, key, val]{data_[key] = val; });
-    }
-
+        q_.async( [data_ = data_, key, val] { 
+            (*data_)[key] = val; }
+        );
+    } 
     // Bucket set: only possible we have the queue, but with mutex lock, it's not possible
-    void set(const vector<pair<string, string>>& kvs) {
-        sq_.async([this, kvs]{
-            data_.insert(begin(kvs), end(kvs));
-        });
+    void set(vector<pair<string, string>> kvs) {
+        q_.async( [data_ = data_, kvs = move(kvs)] { 
+            data_->insert(make_move_iterator(begin(kvs)), make_move_iterator(end(kvs))); }
+        );
     }
 };
 
@@ -145,25 +165,25 @@ cow: copy-on-write pattern: lock-free for Read-heavy / mostly immutable registry
 */
 
 class registry_cow {
-    using map = unordered_map<string, string>;
-    atomic<shared_ptr<map>> data_{ make_shared<map>() }; // atomic<shared_ptr> C++20
-    serial_queue sq_;
+    using map_t = unordered_map<string, string>;
+    atomic<shared_ptr<map_t>> data_{make_shared<map_t>()};
+    serial_queue q_;
 
 public:
     optional<string> get(const string& key) {
-        auto snap = data_.load(memory_order_acquire);
-        auto it = snap->find(key);
-        if (it!=snap->end()) return it->second;
+        auto snapshot = data_.load(memory_order_acquire);
+        auto it = snapshot->find(key);
+        if(it != snapshot->end()) return it->second;
         return nullopt;
     }
 
-     void set(const string& k, const string& v) {
-        sq_.async([this, k, v]{
-            auto new_snap = make_shared<map>(*data_.load(memory_order_acquire)); // we can only do copy, because read could happen any time.
-            // this read here can be relaxed because our queue is serial which guarentees the read is after prev write
-            (*new_snap)[k] = v;
-            data_.store(new_snap, memory_order_release);
+    void set(const string& key, const string& val) {
+        q_.async( [this, key, val] { 
+            auto snapshot = data_.load(memory_order_acquire); //can be relaxed 
+            // this read here can be relaxed because our queue is serial which guarentees the read is after prev write           
+            auto copy = make_shared<map_t>(*snapshot);// we can only do copy, because read could happen any time.
+            (*copy)[key] = val; 
+            data_.store(copy, memory_order_release); // to pair the reader
         });
-     }
-    
+    } 
 };
