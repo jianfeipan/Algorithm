a43514d173e202ce224f260c2aefa78c2af2c41f
jianfeipan <jianfei.pan.tb@gmail.com>
2026-07-26 17:51:19 +0200

    cleaning

--- cpp/concurrency/2_task_system.cpp
+++ cpp/concurrency/2_task_system.cpp
@@ -41,7 +41,7 @@ public:
     void push(Task&& task) {
         {
             lock_guard<mutex> g(mtx_);
-            if (done_) throw runtime_error("Push to stopped queue.");
+            if(done_) throw runtime_error("push task to a stopped queue.");
             q_.emplace_back(move(task));
         }
         cv_.notify_one();
@@ -49,9 +49,9 @@ public:
 
     void pop(Task& task) {
         unique_lock<mutex> lock(mtx_);
-        // while (q_.empty() && !done_) cv_.wait(lock);
+        // while(q_.empty() && !done_) cv_.wait(lock);
         cv_.wait(lock, [this]{ return !q_.empty() || done_; });
-        if (q_.empty() && done_) return;
+        if(q_.empty() && done_) return;
         task = move(q_.front());
         q_.pop_front();
     }
@@ -60,9 +60,9 @@ public:
     bool try_push(Task&& task) {
         {
             unique_lock<mutex> lock(mtx_, try_to_lock);
-            if (!lock) return false;
+            if(!lock) return false;
 
-            if (done_) throw runtime_error("Push to stopped queue.");
+            if(done_) throw runtime_error("push task to a stopped queue.");
             q_.emplace_back(move(task));
         }
         cv_.notify_one();
@@ -90,26 +90,26 @@ class task_system_single_queue {
     const static unsigned capacity_{4};
     notification_queue q_;
     vector<thread> workers_;
-    bool done_{false};
 
     void run() {
-        while (!done_) {
-            Task task;
+        while(true) {
+            Task task; 
             q_.pop(task);
-            if (!task) return;
+            if(!task) return;
             task();
         }
     }
 public:
     task_system_single_queue() {
-        for ( unsigned index = 0; index < capacity_; ++index) {
+        workers_.reserve(capacity_);
+        for(unsigned index = 0; index < capacity_; ++index) {
             workers_.emplace_back([this]{ run(); });
         }
     }
-    
+
     ~task_system_single_queue() {
         q_.stop();
-        for ( auto& worker : workers_) { if ( worker.joinable()) worker.join(); }
+        for(auto& worker : workers_) if(worker.joinable()) worker.join();
     }
 
     task_system_single_queue(const task_system_single_queue&) = delete;
@@ -135,28 +135,27 @@ class task_system_multi_queue {
     const static unsigned capacity_{4};
     vector<notification_queue> qs_;
     vector<thread> workers_;
-    bool done_{false};
-
     atomic<unsigned> index_{0}; // scheduler -> which queue to go
 
     void run(unsigned index) {
-        while (!done_) {
+        while(true) {
             Task task;
             qs_[index].pop(task);
-            if (!task) return;
+            if(!task) return;
             task();
         }
     }
 public:
-    task_system_multi_queue() : qs_(capacity_){
-        for (unsigned index = 0; index < capacity_; ++index) {
-            workers_.emplace_back([this, index] { run(index); });
+    task_system_multi_queue() : qs_(capacity_) {
+        workers_.reserve(capacity_);
+        for(unsigned index = 0; index < capacity_; ++index) {
+            workers_.emplace_back([this, index]{ run(index); });
         }
     }
-    
-    ~task_system_multi_queue() {
-        for ( auto& q : qs_) q.stop();
-        for ( auto& worker : workers_) { if ( worker.joinable()) worker.join(); }
+
+     ~task_system_multi_queue() {
+        for(auto& q : qs_) q.stop();
+        for(auto& worker : workers_) if(worker.joinable()) worker.join();
     }
 
     task_system_multi_queue(const task_system_multi_queue&) = delete;
@@ -198,45 +197,44 @@ in order to steal, we need the notification_queue to beable to try_push/try_pop
 
 
     void run(unsigned index) {
-        while (true) {
+        while(true) {
             Task task;
-            for (unsigned i = 0; i<capacity_; ++i) {
-                if(qs_[i].try_pop(task)) break;
+            for (unsigned i = 0; i < capacity_; ++i) {
+                if(qs_[(index + i)%capacity_].try_pop(move(task))) break;
             }
-            if (!task) qs_[index].pop(task); // all queue locked, fall back to wait to my queue
-            if (!task) return;
+            if(!task) qs_[index].pop(task); // fallback
+            if(!task) return;
             task();
         }
     }
 public:
-    task_system_multi_queue_stealing() : qs_(capacity_){
-        for (unsigned index = 0; index < capacity_; ++index) {
-            workers_.emplace_back([this, index] { run(index); });
-        } 
+    task_system_multi_queue_stealing() : qs_(capacity_) {
+        workers_.reserve(capacity_);
+        for(unsigned index = 0; index < capacity_; ++index) {
+            workers_.emplace_back([this, index]{ run(index); });
+        }
     }
-    
-    ~task_system_multi_queue_stealing() {
-        for ( auto& q : qs_) q.stop();
-        for ( auto& worker : workers_) worker.join();
+
+     ~task_system_multi_queue_stealing() {
+        for(auto& q : qs_) q.stop();
+        for(auto& worker : workers_) if(worker.joinable()) worker.join();
     }
 
     task_system_multi_queue_stealing(const task_system_multi_queue_stealing&) = delete;
     task_system_multi_queue_stealing& operator=(const task_system_multi_queue_stealing&) = delete;
 
     void async(Task&& task) {
-        auto cur = index_.fetch_add(1, memory_order_relaxed);
-        // try to push to a free queue
-        for (unsigned i = 0; i<capacity_ * K; ++i) {
-            if (qs_[ (cur+i)%capacity_].try_push(move(task))) return;
+        const auto cur = index_.fetch_add(1, memory_order_relaxed);
+        for(unsigned i = 0; i < capacity_ * K; ++i) {
+            if(qs_[(cur + i) % capacity_].try_push(move(task))) return;
         }
-        // all queue busy, fallback to blocking on my queue.
         qs_[cur % capacity_].push(move(task));
     }
 };
 
 // usage: 
-template<typename Function, typename...Args>
-auto async(task_system_multi_queue_stealing& task_system, Function&& f, Args&&...args) 
+template<typename Function, typename... Args> 
+auto async(task_system_multi_queue_stealing& task_system, Function&& f, Args&&... args)
 -> future<invoke_result_t<Function, Args...>> {
     using return_type = invoke_result_t<Function, Args...>;
 
